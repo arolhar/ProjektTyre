d637b75d07f26fb72e001e8f7ad350db3e6751bc
arolhar <zurexx@gmail.com>
2017-03-17 10:03:58 +0100

    Dodano polskie nazwy

--- tyre/apps.py
+++ tyre/apps.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 from django.apps import AppConfig
 
 
 class TyreConfig(AppConfig):
     name = 'tyre'
+    verbose_name = 'Opony'
--- tyre/models.py
+++ tyre/models.py
@@ -3,10 +3,10 @@ from django.utils import timezone
 
 
 class Symbol(models.Model):
-    symbol = models.CharField(max_length=50)
-    description = models.TextField()
+    symbol = models.CharField(max_length=50, verbose_name="Symbol")
+    description = models.TextField(verbose_name="Opis")
     created_date = models.DateTimeField(
-            default=timezone.now)
+            default=timezone.now, verbose_name="Data dodania")
 
     def create(self):
         self.created_date = timezone.now()
@@ -14,10 +14,14 @@ class Symbol(models.Model):
 
     def __str__(self):
         return self.symbol
+    
+    class Meta:
+        verbose_name = 'Dodatkowe oznaczenie'
+        verbose_name_plural = 'Dodatkowe oznaczenia'
     
 class Capacity(models.Model):
-    symbol = models.CharField(max_length=10)
-    capacity = models.CharField(max_length=10)
+    symbol = models.CharField(max_length=10, verbose_name="Symbol")
+    capacity = models.CharField(max_length=10, verbose_name="Maksymalna nośność [kg]")
     created_date = models.DateTimeField(
             default=timezone.now)
 
@@ -27,10 +31,14 @@ class Capacity(models.Model):
 
     def __str__(self):
         return self.symbol
+    
+    class Meta:
+        verbose_name = 'Indeks nośności'
+        verbose_name_plural = 'Indeksy nośności'
     
 class Speed(models.Model):
-    symbol = models.CharField(max_length=10)
-    speed = models.CharField(max_length=10)
+    symbol = models.CharField(max_length=10, verbose_name="Symbol")
+    speed = models.CharField(max_length=10, verbose_name="Maksymalna prędkość [km/h]")
     created_date = models.DateTimeField(
             default=timezone.now)
 
@@ -40,12 +48,16 @@ class Speed(models.Model):
 
     def __str__(self):
         return self.symbol
+    
+    class Meta:
+        verbose_name = 'Indeks prędkości'
+        verbose_name_plural = 'Indeksy prędkości'
     
 class Label(models.Model):
-    symbol = models.CharField(max_length=5)
-    rollingResistance = models.CharField(max_length=100)
-    wetGrip = models.CharField(max_length=100)
-    externalNoise = models.TextField()
+    symbol = models.CharField(max_length=5, verbose_name="Symbol")
+    rollingResistance = models.CharField(max_length=100, verbose_name="Opory toczenia [kg/t]")
+    wetGrip = models.CharField(max_length=100, verbose_name="Przyczepność na mokrej nawierzchni [%]")
+    externalNoise = models.TextField(verbose_name="Hałas zewnętrzny")
 
     created_date = models.DateTimeField(
             default=timezone.now)
@@ -57,11 +69,15 @@ class Label(models.Model):
     def __str__(self):
         return self.symbol
     
+    class Meta:
+        verbose_name = 'Etykieta opony'
+        verbose_name_plural = 'Etykiety opon'
+    
 class Companies(models.Model):
-    name = models.CharField(max_length=200)
-    headquarters = models.CharField(max_length=200)
-    brands = models.TextField()
-    factory = models.TextField()
+    name = models.CharField(max_length=200, verbose_name="Nazwa")
+    headquarters = models.CharField(max_length=200, verbose_name="Siedziba")
+    brands = models.TextField(verbose_name="Produkowane marki")
+    factory = models.TextField(verbose_name="Fabryki")
 
     created_date = models.DateTimeField(
             default=timezone.now)
@@ -72,3 +88,7 @@ class Companies(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name = 'Koncern'
+        verbose_name_plural = 'Koncerny'
